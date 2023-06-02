816b0ec76d219251978f402bada32c73d02841fb
mohanram <mohanraam2000@gmail.com>
2023-06-02 11:44:00 +0530

    FirstFile Trail2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import { HelloWorld } from './Components/HelloWorld';
+import  Hello from './Components/Hello';
 
 function App() {
   return (
     <div className="App">
-      <HelloWorld></HelloWorld>
+      <Hello></Hello>
     </div>
   );
 }
--- src/Components/helloWorld.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const HelloWorld = () => {
-  return (
-    <div>HelloWorld, this is a modification to the ci / cd pipeline</div>
-  )
-}
-
-export default HelloWorld
